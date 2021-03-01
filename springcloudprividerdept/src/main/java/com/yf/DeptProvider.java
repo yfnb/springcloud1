75a8cd551d9dfc3cc02e8053b3fa4c3891fd8162
yfnb <935821638@qq.com>
2021-03-01 13:09:25 +0800

    springcloud初步搭建运行01

--- springcloudprividerdept/src/main/java/com/yf/DeptProvider.java
+++ springcloudprividerdept/src/main/java/com/yf/DeptProvider.java
@@ -2,10 +2,13 @@ package com.yf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //在服务启动后，自动注册到eureka中
+@EnableDiscoveryClient  //服务发现
 public class DeptProvider {
-
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider.class,args);
     }
